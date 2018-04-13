481833622dd7cfd7c5f68c2062b95c3597376843
Maximilian Bauer <bauer.maximilian@web.de>
2018-04-13 20:51:53 +0200

    New added files are included in the install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@
 
 ln -s ./dotfiles/.emacs.d ../.emacs.d
 ln -s ./dotfiles/.spacemacs.d ../.spacemacs.d
+ln -s ./dotfiles/.profile ../.profile
+ln -s ./dotfiles/.bash_logout ../.bash_logout
+ln -s ./dotfiles/.bashrc ../.bashrc 
