4e06094b40a676dd3f6eb92d207699deb9bb6c8f
Maximilian Bauer <bauer.maximilian@web.de>
2018-04-18 20:53:01 +0200

    Auto-installing important packages for Debian

--- firstInstallDebian.sh
+++ firstInstallDebian.sh
@@ -1,5 +1,96 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
+# use the bash as default "sh", fixed some problems
+# with e.g. third-party scripts
+#sudo ln -sf /bin/bash /bin/sh
+
+function ask_install() {
+  echo
+  echo
+  read -p"$1 (y/n) " -n 1
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    return 1
+  else
+    return 0
+  fi
+
+}
+
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+  echo "This script must be run as root"
+  echo "Plese use sudo or su"
+  exit 1
+fi
+
+# use aptitude in the next steps ...
+if [ \! -f $(whereis aptitude | cut -f 2 -d ' ') ] ; then
+  apt-get install aptitude
+fi
+
+# update && upgrade
+ask_install "upgrade your system?"
+if [[ $? -eq 1 ]]; then
+  aptitude update
+  aptitude upgrade
+fi
+
+aptitude install \
+  `# default for many other things` \
+  tmux \
+  build-essential \
+  autoconf \
+  make \
+  cmake \
+  mktemp \
+  dialog \
+  `# unzip, unrar etc.` \
+  zip \
+  unzip \
+  tar \
+  `# quickly find files on the filesystem based on their name` \
+  mlocate \
+  locales \
+  `# interactive processes viewer` \
+  htop \
+  `# interactive I/O viewer` \
+  iotop \
+  tree \
+  `# disk usage viewer` \
+  rsync \
+  whois \
+  vim \
+  `# GNU bash` \
+  bash \
+  bash-completion \
+  `# get files from web` \
+  wget \
+  curl \
+  `# repo-tools`\
+  git \
+  `# usefull tools` \
+  stow \
+  nodejs \
+  npm \
+  lynx \
+  python \
+  python-pip \
+  python-dev
+
+# try zsh?
+read -p "Do you want to use the zsh-shell? (y/n) " -n 1 yesOrNo
+echo
+if [[ $yesOrNo =~ ^[Yy]$ ]]; then
+  sudo aptitude install zsh
+  chsh -s $(which zsh)
+fi
+
+# clean downloaded and already installed packages
+aptitude -v clean
+
+# Symlinking dotfiles via GNU stow
+echo "Symlinking dotfiles via GNU stow"
 for i in profile bash emacs zsh 
 do
  stow $i 
