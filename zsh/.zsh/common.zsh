0ff1d1a115d53f865f996ffd0c0e089ab298305f
Maximilian Bauer <bauer.maximilian@web.de>
2018-04-18 21:45:26 +0200

    Using agnoster theme and acticate vi-mode for zsh

--- zsh/.zsh/common.zsh
+++ zsh/.zsh/common.zsh
@@ -1,15 +1,2 @@
-# Lines configured by zsh-newuser-install
-HISTFILE=~/.histfile
-HISTSIZE=1000
-SAVEHIST=1000
-bindkey -v
-# End of lines configured by zsh-newuser-install
-# The following lines were added by compinstall
-zstyle :compinstall filename '/cygdrive/c/Users/SESA377571/.zshrc'
-
-autoload -Uz compinit
-compinit
-# End of lines added by compinstall
-
 # Greeter message at shell start
 screenfetch
