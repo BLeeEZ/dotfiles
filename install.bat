eab3e84d7be8c503bff15ef02003bbd89e5bd668
Maximilian Bauer <bauer.maximilian@web.de>
2018-04-18 08:34:36 +0200

    Adjusting windows install

--- install.bat
+++ install.bat
@@ -1,2 +1,2 @@
-mklink /J ".emacs.d" "dotfiles/.emacs.d"
-mklink /J ".spacemacs.d" "dotfiles/.spacemacs.d"
+mklink /J "../.emacs.d" "../dotfiles/emacs/.emacs.d"
+mklink /J "../.spacemacs.d" "../dotfiles/emacs/.spacemacs.d"
