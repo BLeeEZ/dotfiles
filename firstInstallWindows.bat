f72f3e0c964c6e33a5411203b1e3486586112b2e
Maximilian Bauer <bauer.maximilian@web.de>
2018-04-24 13:47:35 +0200

    Creating also a symlink to proxy conf for qutebrowser

--- firstInstallWindows.bat
+++ firstInstallWindows.bat
@@ -2,3 +2,4 @@ mklink /J "../.emacs.d" "../dotfiles/emacs/.emacs.d"
 mklink /J "../.spacemacs.d" "../dotfiles/emacs/.spacemacs.d"
 
 mklink "C:/Users/%username%/AppData/Roaming/qutebrowser/config/config.py" "C:/Users/%username%/dotfiles/qutebrowser/config.py"
+mklink "C:/Users/%username%/AppData/Roaming/qutebrowser/config/proxy_conf.py" "C:/Users/%username%/dotfiles/qutebrowser/proxy_conf.py"
