8e9f49edbc259c4aa5458236b8df77c03ca895ec
Maximilian Bauer <bauer.maximilian@web.de>
2018-05-07 21:29:11 +0200

    Using fontconfig to control fonts

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -145,7 +145,7 @@ values."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Hack"
+   dotspacemacs-default-font '("monospace"
                                :size 12
                                :weight normal
                                :width normal
