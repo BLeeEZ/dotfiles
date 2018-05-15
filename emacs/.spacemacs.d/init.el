fd471e5fb9b238b52842bb7912b5436610499164
Maximilian Bauer <bauer.maximilian@web.de>
2018-05-15 12:43:25 +0200

    Add version-control for colorful git diffs

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -54,7 +54,7 @@ values."
             shell-default-term-shell "/bin/zsh")
      ;; spell-checking
      ;; syntax-checking
-     ;; version-control
+     version-control
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
