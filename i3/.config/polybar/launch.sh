c7d4d0d67ed73fba81cbb1d8eda7cb0e0e323fc7
Maximilian Bauer <bauer.maximilian@web.de>
2018-05-23 10:28:47 +0200

    Able to run polybar on multimonitor setup

--- i3/.config/polybar/launch.sh
+++ i3/.config/polybar/launch.sh
@@ -7,6 +7,6 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar example
+for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar example -c ~/.config/polybar/config & done
 
 echo "Bars launched..."
