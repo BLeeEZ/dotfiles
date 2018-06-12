fe9884912a1d20c9bd2887560bc68e4a425b6511
Maximilian Bauer <bauer.maximilian@web.de>
2018-06-12 16:12:44 +0200

    Removing unused firstInstall* scripts

--- firstInstallWindows.bat
+++
@@ -1,5 +0,0 @@
-mklink /J "../.emacs.d" "../dotfiles/emacs/.emacs.d"
-mklink /J "../.spacemacs.d" "../dotfiles/emacs/.spacemacs.d"
-
-mklink "%HOME%/AppData/Roaming/qutebrowser/config/config.py" "%HOME%/dotfiles/qutebrowser/.config/qutebrowser/config.py"
-mklink "%HOME%/AppData/Roaming/qutebrowser/config/proxy_conf.py" "%HOME%/dotfiles/qutebrowser/.config/qutebrowser/proxy_conf.py"
