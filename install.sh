537502fd76124af51eb375d91b22b875c980215d
Maximilian Bauer <bauer.maximilian@web.de>
2019-06-26 21:16:07 +0200

    Adding progs to be installed

---
+++ install.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+  echo "This script must be run as root"
+  echo "Plese use sudo or su"
+  exit 1
+fi
+
+function ask_install() {
+  read -p"$1 (y/n) " -n 1
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    return 1
+  else
+    return 0
+  fi
+}
+
+function ask_install_package_selection() {
+  ask_install "Install $1 packages?"
+  if [[ $? -eq 1 ]]; then
+    for x in $(cat ./archProgs.csv | grep -G ",$1," | awk -F, {'print $2","$1'})
+      do
+        installCmd=$(echo "$x,$SUDO_USER" | awk -F, {'
+          if ($1 == "pacman")
+            print $1" -S --needed "$2;
+          else
+            print "sudo -u "$3" "$1" -S --needed "$2;
+        '})
+        echo $installCmd
+        $installCmd
+      done
+  fi
+}
+
+ask_install "Upgrade your system?"
+if [[ $? -eq 1 ]]; then
+  pacman -Syu
+fi
+
+ask_install_package_selection basic
+ask_install_package_selection common
+
+ask_install "Install zsh-shell?"
+if [[ $? -eq 1 ]]; then
+  pacman -S --needed zsh
+  echo Change default shell to zsh
+  sudo -u $SUDO_USER chsh -s $(which zsh)
+  stow zsh
+fi
+
+ask_install "Symlinking dotfiles via GNU stow?"
+if [[ $? -eq 1 ]]; then
+  for i in bash profile bin ranger tmux vim
+  do
+    echo stow $i
+    stow $i
+  done
+fi
